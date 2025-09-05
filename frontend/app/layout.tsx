5b6d6e389e8d08e5dd237684e47c43f0ccdaeeb6
thesheriflawal <lawalsherif0000@gmaill.com>
2025-09-05 18:33:23 +0100

    Add backend and frontend folders

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,30 +1,37 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import { Analytics } from "@vercel/analytics/next"
-import { Web3Provider } from "@/lib/web3-provider"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
-import { Suspense } from "react"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import { Web3Provider } from "@/lib/web3-provider";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { Suspense } from "react";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SalaryStream - Decentralized Salary Streaming Platform",
+  title: "TrustPayroll - Decentralized Salary Streaming Platform",
   description:
     "A blockchain-based platform for organizations to stream salaries to employees based on daily attendance",
-  generator: "v0.app",
-}
+  icons: {
+    icon: "/trustpayroll.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Suspense fallback={null}>
             <Web3Provider>
               {children}
@@ -35,5 +42,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
